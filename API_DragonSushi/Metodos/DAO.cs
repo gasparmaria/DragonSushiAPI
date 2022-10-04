66c21c6486a8a8e8ea31fbd6ea44266662589cde
gasparmaria <maria.eduarda.gaspar07@gmail.com>
2022-10-04 10:20:28 -0300

    métodos de ViewModel

--- API_DragonSushi/Metodos/DAO.cs
+++ API_DragonSushi/Metodos/DAO.cs
@@ -5,6 +5,7 @@ using MySql.Data.MySqlClient;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Web;
 using System.Web.Mvc;
 
@@ -16,31 +17,33 @@ namespace API_DragonSushi.Metodos
 
         //-----------------------------------------------------------------------------------------------------------------------------
         //MÉTODO DE INSERÇÃO: USUÁRIO
-
-       
-
         public void CadastrarUsuario(UsuarioViewModel vmUsuario)
         {
             var strQuery = "";
-            strQuery += string.Format("Call spCadastrarUsuario ('{0}','{1}','{2}','{3}','{4}')", vmUsuario.Pessoa.nomePessoa, vmUsuario.Pessoa.telefone, vmUsuario.Pessoa.cpf, vmUsuario.Usuario.login, vmUsuario.Usuario.senha);
-
+            strQuery += string.Format("CALL spCadastrarUsuario ('{0}','{1}','{2}','{3}','{4}')", 
+                                                                vmUsuario.Pessoa.nomePessoa, 
+                                                                vmUsuario.Pessoa.telefone, 
+                                                                vmUsuario.Pessoa.cpf, 
+                                                                vmUsuario.Usuario.login, 
+                                                                vmUsuario.Usuario.senha);
 
             using (db = new DataBase())
             {
                 db.ExecutaComando(strQuery);
             }
-
-
         }
 
         //-----------------------------------------------------------------------------------------------------------------------------
         //MÉTODO DE INSERÇÃO: FUNCIONÁRIO
         public void CadastrarFuncionario(Usuario usuario, Pessoa pessoa)
-        {
-            
+        {            
             var strQuery = "";
-            strQuery += string.Format("spCadastrarFuncionario ('{0}','{1}','{2}','{3}','{4}')", pessoa.nomePessoa, pessoa.telefone, pessoa.cpf, usuario.login, usuario.senha);
-
+            strQuery += string.Format("CALL spCadastrarFuncionario ('{0}','{1}','{2}','{3}','{4}')", 
+                                                                pessoa.nomePessoa, 
+                                                                pessoa.telefone, 
+                                                                pessoa.cpf, 
+                                                                usuario.login, 
+                                                                usuario.senha);
 
             using (db = new DataBase())
             {
@@ -53,8 +56,7 @@ namespace API_DragonSushi.Metodos
         public void CadastrarCliente(Pessoa pessoa)
         {
             var strQuery = "";
-            strQuery += string.Format("spCadastrarCliente ('{0}','{1}','{2}')", pessoa.nomePessoa, pessoa.telefone, pessoa.cpf);
-
+            strQuery += string.Format("CALL spCadastrarCliente ('{0}','{1}','{2}')", pessoa.nomePessoa, pessoa.telefone, pessoa.cpf);
 
             using (db = new DataBase())
             {
@@ -67,29 +69,31 @@ namespace API_DragonSushi.Metodos
          public void CadastrarComanda(Comanda comanda)
          {
             var strQuery = "";
-            strQuery += string.Format("spCadastrarComanda ({0})", comanda.numMesa);
-
+            strQuery += string.Format("CALL spCadastrarComanda ({0})", comanda.numMesa);
 
             using (db = new DataBase())
             {
                 db.ExecutaComando(strQuery);
             }
          }
+
         //------------------------------------------------------------------------------------------------------------------------------
         //MÉTODO INSERÇÃO: ENDEREÇO
         public void CadastrarEndereco(Endereco endereco, Bairro bairro, Cidade cidade, Estado estado, Rua rua)
         {
             var strQuery = "";
-            strQuery += string.Format("spCadastrarEndereco ({0}, '{1}', '{2}', '{3}', '{4}', '{5}')", endereco.numEndereco, endereco.descrEndereco, rua.rua, bairro.bairro, cidade.cidade, estado.idEstado);
-
+            strQuery += string.Format("CALL spCadastrarEndereco ({0}, '{1}', '{2}', '{3}', '{4}', '{5}')", 
+                                                            endereco.numEndereco, 
+                                                            endereco.descrEndereco, 
+                                                            rua.rua, 
+                                                            bairro.bairro, 
+                                                            cidade.cidade, 
+                                                            estado.idEstado);
 
             using (db = new DataBase())
             {
                 db.ExecutaComando(strQuery);
             }
         }
-
-
-
     }
 }
